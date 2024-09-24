344e681cedfa5e3cb0e70b93eec83cf9ac593487
shynishyni <shynisalamon@gmail.com>
2024-09-24 14:42:14 +0530

    Image commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import UserDetailsTable
 from .models import TurfDetails
+from .models import TurfImage
 
 class UserSreializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +11,16 @@ class UserSreializer(serializers.ModelSerializer):
 class TurfSerializer(serializers.ModelSerializer):
     class Meta:
         model = TurfDetails
-        fields ='__all__'
+        fields = [
+            'turf_name', 'turf_address', 'turf_city', 'turf_state',
+            'turf_zip_code', 'turf_latitude', 'turf_longitude', 'turf_type',
+            'surface_type', 'size', 'capacity', 'status', 'opening_time', 
+            'closing_time', 'closed_days', 'hourly_rate', 'peak_hour_rate', 
+            'discount', 'owner_name', 'turf_contact_phone', 'turf_contact_email',
+            'images'
+        ]
+
+class TurfImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TurfImage
+        fields = ['image']
--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('login/',views.login,name='login'),
     path('turf/',views.turf,name='turf')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
