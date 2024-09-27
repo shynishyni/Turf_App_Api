6c0d6bc59e6f45951e1901ad40d6b94eac657773
shynishyni <shynisalamon@gmail.com>
2024-09-27 15:46:32 +0530

    image in table Commit

--- api/models.py
+++ api/models.py
@@ -71,6 +71,7 @@ class TurfDetails(models.Model):
     owner_name = models.CharField(max_length=200)
     turf_contact_phone = models.CharField(max_length=10)
     turf_contact_email = models.EmailField()
+    image = models.ImageField(upload_to='turf_images/')
 
     def __str__(self):
         return self.turf_name  # Updated __str__ method to use turf_name
--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import os
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
