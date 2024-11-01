a27dfa89f1214643439ed7579be9834672e01bb3
shynishyni <shynisalamon@gmail.com>
2024-11-01 11:28:35 +0530

    Settings Commit

--- project/settings.py
+++ project/settings.py
@@ -73,9 +73,7 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = [
-http://192.168.1.9:8081
-]
+CORS_ALLOWED_ORIGINS = ['http://192.168.1.9:8081']
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
